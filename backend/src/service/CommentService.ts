dd8b6fd4550ff881b72878a4d875001d1f113bb9
ayush-udaan <99483366+ayush-udaan@users.noreply.github.com>
2023-06-28 22:40:51 +0530

    add logger in CommentService

--- backend/src/service/CommentService.ts
+++ backend/src/service/CommentService.ts
@@ -2,6 +2,7 @@ import { prisma } from "../config";
 
 class CommentService {
   async getCommentsByPostId(postId: string, parentId: string | null) {
+    console.log(`get comment request with postId ${postId}`)
     return prisma.comment.findMany({
       where: {
         postId,
